3e82ffc762c2c45e2167727bfbe2403fd756a30d
cherry-mittal <cherrmittal@gmail.com>
2025-06-17 22:31:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ async function openTerminal() {
     await delay(200);
     await createText("Need help? Just press Enter and we’ll pretend to know what we’re doing. 🤫✨");
     await delay(200);
-    await createText("You can now interact with the Terminal");
+    //await createText("You can now interact with the Terminal");
     await createCode("Type help", "for a list of commands");
     await delay(500);
     new_line();
